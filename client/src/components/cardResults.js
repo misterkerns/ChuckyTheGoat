97cb2d76830a47f1da1263c0b46ee6f6ef3dee74
Kyle Moore <kyledavid716@gmail.com>
2022-12-08 00:01:47 -0800

    tinkering

--- client/src/components/cardResults.js
+++ client/src/components/cardResults.js
@@ -2,7 +2,7 @@ import React from 'react';
 export default function CardResults() {
     return(
         <div>
-        <h1>results</h1>
-    </div>
+            <h1>results</h1>
+        </div>
     )
 }
